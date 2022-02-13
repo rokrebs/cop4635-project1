8a8fd4a415040c19970d422758a785aca2382ec8
Rocio Krebs <54292420+rokrebs@users.noreply.github.com>
2022-02-13 00:12:09 -0600

    with bugs

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
-//
-//  main.cpp
-//  Project1
-//
-//  Created by Rocio Krebs on 2/10/22.
-//
+/*
+ * @autor Rocio Krebs
+ *
+ */
 
 #include "paketHandler.hpp"
 #include <iostream>
@@ -15,75 +13,76 @@
 #include <unistd.h>
 
 // read a file into memory
-#include <fstream>  // std::ifstream
+//#include <fstream>  // std::ifstream
 #include <iostream> // std::cout
+
+#include <sys/wait.h>
+#include <unistd.h>
 
 #define PORT 60060
 
 using namespace std;
 
-int main(int argc, const char *argv[]) {
+int server_fd;
+int new_socket;
+long valread;
+int waitSize = 16;
+struct sockaddr_in address;
+int addressLen = sizeof(address);
 
-  int server_fd;
-  int new_socket;
-  long valread;
-  int waitSize = 16;
-  struct sockaddr_in address;
-  int addressLen = sizeof(address);
-
-  // creating socket file descriptor
-  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("Error: Listen socked failed!");
-    exit(1);
-  } else {
-    printf("Server started.");
-  }
-
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = htons(PORT);
-
-  memset(address.sin_zero, '\0', sizeof address.sin_zero);
-        printf("Bind:\n");
-  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-    perror("Error: binding failed!");
-    exit(1);
-  } else {
-    printf("Server ready.");
-  }
-printf("Listen:\n");
-  if (listen(server_fd, waitSize) < 0) {
-    perror("Error: listetning failed!");
-    exit(1);
-  } else {
-    printf("Server listening and waiting for client request...");
-  }
-printf("Go long\n");
-  // handle the connection
-  for (;;) { // run forever
-        printf("New Conn Made:\n");
-    // accept connections from client
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                             (socklen_t *)&addressLen)) < 0) {
-      perror("Error: accepting failed");
-      exit(1);
+void server();
+void server() {
+    // creating socket file descriptor
+    cout << "Server started." << endl;
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Error: Listen socked failed!");
+        exit(1);
     }
 
-    // Data transfer section
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
 
-    char buffer[30000];
-    while ((valread = read(new_socket, buffer, 30000)) > 0) {
-      // create string and return that string in write.
-      paketHandler paket(buffer);
-	   printf("%s\n",buffer);
-      std::string msg = paket.printMsg();
-        printf("%s\n",(char*)msg.c_str());  
-      write(new_socket, (char *)msg.c_str(),(int)msg.size());
-      //  printf("\n-------------Hello message sent--------------");
+    memset(address.sin_zero, '\0', sizeof address.sin_zero);
+    printf("Bind:\n");
+    if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
+        perror("Error: binding failed!");
+        exit(1);
     }
+    cout << "Server is listening" << endl;
+    if (listen(server_fd, waitSize) < 0) {
+        perror("Error: listetning failed!");
+        exit(1);
+    }
+    cout << "Server listening and waiting for client request..." << endl;
+}
 
-    close(new_socket);
-  }
 
-  return 0;
+int main() {
+    server();
+    // handle the connection
+    while (1) { // run forever
+
+        // accept connections from client
+        if ((new_socket = accept(server_fd, (struct sockaddr *) &address,
+                                 (socklen_t * ) & addressLen)) < 0) {
+            perror("Error: accepting failed");
+            exit(1);
+        }
+        if (fork() == 0) {
+            cout << "New client is connected" << endl;
+            // Data transfer section
+            char buffer[30000];
+            while ((valread = read(new_socket, buffer, 30000)) > 0) {
+                // create string and return that string in write.
+                paketHandler paket(buffer);
+                std::string msg = paket.printMsg();
+                write(new_socket, (char *) msg.c_str(), (int) msg.size());
+            }
+            close(new_socket);
+            exit(0);
+        } else {
+            cout << "waiting for more clients" << endl;
+        }
+    }
 }
--- paketHandler.cpp
+++ paketHandler.cpp
@@ -1,109 +1,131 @@
+/*
+ * @autor Rocio Krebs
+ *
+ */
+
 #include "paketHandler.hpp"
 
-int a_l = 0;
-
 paketHandler::paketHandler(char *buffer) {
-  responseToClient = "";
-  splitChar(buffer);
+    responseToClient = "";
+    splitChar(buffer);
 }
 
 void paketHandler::splitChar(char *buffer) {
-  std::string item;
-  std::stringstream ss(buffer);
+    std::string item;
+    std::stringstream ss(buffer);
 
-  while (ss >> item) {
-    content.push_back(item);
-  }
-  setRespose();
+    while (ss >> item) {
+        content.push_back(item);
+    }
+    setResponse();
 }
 
-void paketHandler::setRespose() {
-  int i = 0;
-  r.method = content.at(i++);
-  r.uri = content.at(i++);
-  r.protocolV = content.at(i);
-  setRequest();
+void paketHandler::setResponse() {
+    int i = 0;
+    r.method = content.at(i++);
+    r.uri = content.at(i++);
+    r.protocolV = content.at(i);
+    setRequest();
 }
 
 void paketHandler::setRequest() {
-  a.protocolV = r.protocolV;
-  setTypeStatus();
+    a.protocolV = r.protocolV;
+    setTypeStatus();
 }
 
+/*
+ * function to set the type and the correspoing status of the response
+ */
 void paketHandler::setTypeStatus() {
-  if (r.uri == "/index.html") {
-    a.status = "200";
-    a.msg = "OK";
-	readFile(r.uri);
-    setHtml();
-	return;
-  } else if (r.uri.compare("/dragon.jpeg") == 0) {
-    a.status = "200";
-    a.msg = "OK";
-	readFile(r.uri);
-    setPicture();
-	return;
-  } else {
-	a.length = 13;
-    a.status = "404";
-    a.msg = "NOT FOUND\n";
-	setErr();
-	return;
-  }
+    if (r.uri == "/index.html") {
+        a.status = "200";
+        a.msg = "OK";
+        readFile(r.uri);
+        setHtml();
+        return;
+    } else if (r.uri == "/working.html") {
+        a.status = "200";
+        a.msg = "OK";
+        readFile(r.uri);
+        setHtml();
+        return;
+    } else if (r.uri.compare("/dragon.jpeg") == 0) {
+        a.status = "200";
+        a.msg = "OK";
+        readFile(r.uri);
+        setPicture();
+        return;
+    } else {
+        a.status = "404";
+        a.msg = "NOT FOUND\n";
+        r.uri = "/fake.html";
+        readFile(r.uri);
+        setErr();
+        return;
+    }
 }
 
+/*
+ * function to set a picture ready for the client
+ */
 void paketHandler::setPicture() {
-  stringstream ss;
-  ss << a.protocolV << " " << a.status << " " << a.msg << "\n";
-  ss << "Content-Type: image/jpeg\n";
-  ss << "Content-Length: ";
-  ss << a.length;
-  ss << "\n\n";
-  ss << a.content;
-  responseToClient = ss.str();
+    stringstream ss;
+    ss << a.protocolV << " " << a.status << " " << a.msg << "\n";
+    ss << "Content-Type: image/jpeg\n";
+    ss << "Content-Length: ";
+    ss << a.length;
+    ss << "\n\n";
+    ss << a.content;
+    responseToClient = ss.str();
+    cout << "sent picture" << endl;
 }
+
+/*
+ * function to set the html for the client
+ */
 void paketHandler::setHtml() {
-  stringstream ss;
-  ss << a.protocolV << " " << a.status << " " << a.msg << "\n";
-  ss << "Content-Type: text/html\n";
-  ss << "Content-Length: ";
-  ss << a.length;
-  ss << "\n\n";
-  ss << a.content;
-  printf("%s\n",(char*)(ss.str()).c_str());
-  responseToClient = ss.str();
+    stringstream ss;
+    ss << a.protocolV << " " << a.status << " " << a.msg << "\n";
+    ss << "Content-Type: text/html\n";
+    ss << "Content-Length: ";
+    ss << a.length;
+    ss << "\n\n";
+    ss << a.content;
+    responseToClient = ss.str();
+    cout << "sent html file" << endl;
 }
+
+/*
+ * function to set the error response for the client
+ */
 void paketHandler::setErr() {
-  stringstream ss;
-  ss << a.protocolV << " " << a.status << " " << a.msg << "\n";
-  ss << "Content-Type: text/plain\n";
-  ss << "Content-Length: 13\n";
-  ss << "\n";
-  ss << "404 Not Found\n";
-  responseToClient = ss.str();
+    stringstream ss;
+    ss << a.protocolV << " " << a.status << " " << a.msg << "\n";
+    ss << "Content-Type: texte/html\n";
+    ss << "Content-Length: ";
+    ss << a.length;
+    ss << "\n\n";
+    ss << a.content;
+    responseToClient = ss.str();
+    cout << "Error: Not found" << endl;
 }
 
+/*
+ * function to read each content of the requested file
+ */
 void paketHandler::readFile(string location) {
-  location = location.erase(0, 1);
-	//string path = location;
-  ifstream is(location, ifstream::binary);
-  if (is) {
-    /* // get length of file:
-     is.seekg(0, is.end);
-     a.length = is.tellg();
-     is.seekg(0, is.beg);
-
-     char *buffer = new char[a.length];
-     // read data as a block:
-     is.read(buffer, a.length);
-     return buffer;*/
-    std::stringstream fs;
-    fs << is.rdbuf();
-    std::string o = fs.str();
-	is.close();
-	a.length = (int)o.size();
-    a.content = o;
-  }
+    location = location.erase(0, 1);
+    ifstream is(location, ifstream::binary);
+    if (is) {
+        std::stringstream fs;
+        fs << is.rdbuf();
+        std::string o = fs.str();
+        is.close();
+        a.length = (int) o.size();
+        a.content = o;
+    }
 }
 
-string paketHandler::printMsg() { return responseToClient; }
+string paketHandler::printMsg() {
+    return responseToClient;
+}
